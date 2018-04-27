10330596e536c44fb6a05b79334ea24efb5fc4f9
Brett Wagner <brett@madhouse.org>
2018-04-27 15:45:56 -0700

    bs60

--- keyboards/bananasplit/keymaps/sleepybrett/keymap.c
+++ keyboards/bananasplit/keymaps/sleepybrett/keymap.c
@@ -33,7 +33,7 @@
 #define LAYER_HIGH    3
 #define LAYER_ADJ     4
 
-#define KC_LOWR LOWER
+#define KC_LOW  LOWER
 #define KC_RASE RAISE
 #define KC_ADJ  ADJUST
 
@@ -47,31 +47,23 @@ enum custom_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [DEFAULT_LAYER] = KEYMAP_BRETT( \
+  [0] = KEYMAP_BRETT( \
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,       KC_5,   KC_6,     KC_7,     KC_8,   KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_DEL, KC_ESC, \
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,       KC_T,   KC_Y,     KC_U,     KC_I,   KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, \
-    KC_LOWR,KC_A,   KC_S,     KC_D,     KC_F,       KC_G,   KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,  KC_QUOT, KC_ENT, \
+    KC_LOW,   KC_A,   KC_S,     KC_D,     KC_F,       KC_G,   KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,  KC_QUOT, KC_ENT, \
     KC_LSPO,            KC_Z,     KC_X,     KC_C,       KC_V,   KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, KC_RSPC, KC_UP, \
     KC_LCTL,  KC_LALT,  KC_LGUI,            KC_BSPC,    KC_RASE,KC_SPC,         KC_RGUI,KC_RALT,  KC_LEFT,  KC_RIGHT,KC_DOWN \
   ),
 
-  [LAYER_GAME] = KEYMAP_BRETT( \
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,       KC_5,   KC_6,     KC_7,     KC_8,   KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_DEL, KC_ESC, \
+  [1] = KEYMAP_BRETT( \
+    KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,       KC_5,   KC_6,     KC_7,     KC_8,   KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_DEL, KC_GRV, \
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,       KC_T,   KC_Y,     KC_U,     KC_I,   KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, \
-    KC_LOWR,  KC_A,   KC_S,     KC_D,     KC_F,       KC_G,   KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,  KC_QUOT, KC_ENT, \
+    KC_LOW,   KC_A,   KC_S,     KC_D,     KC_F,       KC_G,   KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,  KC_QUOT, KC_ENT, \
     KC_LSFT,            KC_Z,     KC_X,     KC_C,       KC_V,   KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT, KC_UP, \
-    KC_LCTL,  KC_LALT,  KC_LGUI,            KC_BSPC,    KC_RASE,KC_SPC,         KC_RGUI,KC_RALT,  KC_LEFT,  KC_RIGHT,KC_DOWN \
+    KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,    KC_RASE,KC_BSPC,         KC_RALT,KC_RGUI,  KC_LEFT,  KC_RIGHT,KC_DOWN \
   ),
 
-  [LAYER_LOW] = KEYMAP_BRETT( \
-    KC_ESC,   QWERTY,   GAME,     _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, KC_INS, _______, \
-    KC_CAPS,  _______,  _______,  _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, _______, \
-    _______,  _______,  _______,  _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, \
-    _______,            _______,  _______,  _______,    _______, _______, _______, KC_MUTE, KC_VOLD,  KC_VOLU,  KC_MPLY, _______, KC_PGUP, \
-    _______,  _______,  _______,            _______,    _______, _______,          _______, _______,  KC_HOME,  KC_END,  KC_PGDN  \
-  ),
-
-  [LAYER_HIGH] = KEYMAP_BRETT( \
+  [2] = KEYMAP_BRETT( \
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  KC_INS, _______, \
     KC_CAPS,  QWERTY,   GAME,     _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, _______, \
     _______,  _______,  _______,  _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, \
@@ -79,7 +71,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,            _______,    _______, _______,          _______, _______,  KC_HOME,  KC_END,  KC_PGDN  \
   ),
 
-  [LAYER_ADJ] = KEYMAP_BRETT( \
+  [3] = KEYMAP_BRETT( \
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  KC_INS, _______, \
+    KC_CAPS,  QWERTY,   GAME,     _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, _______, \
+    _______,  _______,  _______,  _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, \
+    _______,            _______,  _______,  _______,    _______, _______, _______, KC_MUTE, KC_VOLD,  KC_VOLU,  KC_MPLY, _______, KC_PGUP, \
+    _______,  _______,  _______,            _______,    _______, _______,          _______, _______,  KC_HOME,  KC_END,  KC_PGDN  \
+  ),
+
+  [4] = KEYMAP_BRETT( \
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  KC_INS, _______, \
     KC_CAPS,  QWERTY,   GAME,     _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, _______, \
     _______,  _______,  _______,  _______,  _______,    _______, _______, _______, _______, _______,  _______,  _______, _______, \
@@ -101,41 +101,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<DEFAULT_LAYER);
+        persistent_default_layer_set(1UL<<0);
       }
       return false;
       break;
     case GAME:
       if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<LAYER_GAME);
+        persistent_default_layer_set(1UL<<1);
       }
       return false;
       break;
     case LOWER:
       if (record->event.pressed) {
-        layer_on(LAYER_LOW);
-        update_tri_layer(LAYER_LOW, LAYER_HIGH, LAYER_ADJ);
+        layer_on(2);
+        update_tri_layer(2, 3, 4);
       } else {
-        layer_off(LAYER_LOW);
-        update_tri_layer(LAYER_LOW, LAYER_HIGH, LAYER_ADJ);
+        layer_off(2);
+        update_tri_layer(2, 3, 4);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
-        layer_on(LAYER_HIGH);
-        update_tri_layer(LAYER_LOW, LAYER_HIGH, LAYER_ADJ);
+        layer_on(3);
+        update_tri_layer(2, 3, 4);
       } else {
-        layer_off(LAYER_HIGH);
-        update_tri_layer(LAYER_LOW, LAYER_HIGH, LAYER_ADJ);
+        layer_off(3);
+        update_tri_layer(2, 3, 4);
       }
       return false;
       break;
     case ADJUST:
       if (record->event.pressed) {
-        layer_on(LAYER_ADJ);
+        layer_on(4);
       } else {
-        layer_off(LAYER_ADJ);
+        layer_off(4);
       }
       return false;
       break;
